34b7e290657ab5b9a1e8875a4830f1b4c98b0e5f
SadielO.B <root@ip-172-31-1-51.us-west-1.compute.internal>
2019-01-03 18:10:33 +0000

    Initial commit

--- main.tf
+++ main.tf
@@ -1,16 +1,34 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_key_pair" "deployer" {
-  key_name   = "us-east-1-windows"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcOETTi0uHVRvHRPdTk9clYiPxjyXZWXEdG49wr77bIFLNgI9rrZfE8WXCN6DdOZxLqaTMHZHG4Syjr/dAofKqVOgQooY0Vtw7uRnifOEA4JtkXHeDV/Dc/hUfvsiBH+4M6XM1K4UKPN1fAErvSDoC/yPadA5Izvq4nMwjAqzRnJu+168sKBSziF9VsVLuVGnf2k/dcvOrrVDn3URkkHuok/MBe2WwwkHf8H2Yzuhv6Ohv+h5J1j+Xvpp65KWxdFF6+O52HImR6mPxf8zQisdVBQdmPWyZCTui6P0F98hkBtBH8b/rrZnGGP+guVyRJBDi8A9Tx/figAbgg2/GGVPx"
-}
-
 resource "aws_instance" "example" {
   ami           = "ami-40d28157"
   instance_type = "t2.micro"
-  
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+    
 tags {
     Name = "terraform-example"
   }
 }
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDcOETTi0uHVRvHRPdTk9clYiPxjyXZWXEdG49wr77bIFLNgI9rrZfE8WXCN6DdOZxLqaTMHZHG4Syjr/dAofKqVOgQooY0Vtw7uRnifOEA4JtkXHeDV/Dc/hUfvsiBH+4M6XM1K4UKPN1fAErvSDoC/yPadA5Izvq4nMwjAqzRnJu+168sKBSziF9VsVLuVGnf2k/dcvOrrVDn3URkkHuok/MBe2WwwkHf8H2Yzuhv6Ohv+h5J1j+Xvpp65KWxdFF6+O52HImR6mPxf8zQisdVBQdmPWyZCTui6P0F98hkBtBH8b/rrZnGGP+guVyRJBDi8A9Tx/figAbgg2/GGVPx"
+} 
